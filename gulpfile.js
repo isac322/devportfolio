487db44c94822876b1e491687fb406408086205e
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-04-16 16:16:44 +0900

    update dependencies and project to support pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,46 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var wait = require('gulp-wait');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
 
-gulp.task('scripts', function() {
-    return gulp.src('js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('js'));
+gulp.task('scripts', function () {
+	return gulp.src('js/scripts.js')
+		.pipe(plumber(plumber({
+			errorHandler: function (err) {
+				console.log(err);
+				this.emit('end');
+			}
+		})))
+		.pipe(uglify({
+			output: {
+				comments: '/^!/'
+			}
+		}))
+		.pipe(rename({extname: '.min.js'}))
+		.pipe(gulp.dest('js'));
 });
 
 gulp.task('styles', function () {
-    return gulp.src('./scss/styles.scss')
-        .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+	return gulp.src('./scss/styles.scss')
+		.pipe(wait(250))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('js/scripts.js', gulp.series('scripts'));
-    gulp.watch('scss/styles.scss', gulp.series('styles'));
+gulp.task('views', function () {
+	return gulp.src('index.pug')
+		.pipe(pug())
+		.pipe(rename({extname: '.html'}))
+		.pipe(gulp.dest('.'))
+});
+
+gulp.task('watch', function () {
+	gulp.watch('js/scripts.js', gulp.series('scripts'));
+	gulp.watch('scss/styles.scss', gulp.series('styles'));
+	gulp.watch('index.pug', gulp.series('views'));
 });
